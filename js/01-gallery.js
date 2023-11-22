ab4f792abe0b6619dcc3146701b252c0c795c5ed
pbohdanov <107268135+pbohdanov@users.noreply.github.com>
2023-11-22 17:44:48 +0300

    updated gallery and lightbox files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,75 @@
-import { galleryItems } from './gallery-items.js';
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const listEl = document.querySelector(".gallery");
+
+
+const renderList = (arr, container) =>{ 
+    const markup = arr.map((item) => `<li class="gallery_item"> 
+    <a class="gallery_link" href="${item.original}">
+    <img
+        class="gallery_image"
+        src="${item.preview}"
+        data-source ="${item.original}"
+        alt="${item.description}"
+        width="360"
+        />
+    </a>
+    </li>`).join("");
+    
+    container.insertAdjacentHTML("afterbegin", markup);
+}
+
+
+const imgGalleryOnClick = (event) => { 
+    event.preventDefault();
+
+ if (event.target.nodeName !== "IMG") {
+    return;
+ }
+    const clickedImg = event.target;
+
+    const chosenImgSource = clickedImg.getAttribute("data-source");
+
+    const galleryItem = galleryItems
+        .find(item => item.original === chosenImgSource);
+
+    if (galleryItem) {
+        console.log(galleryItem.original); 
+    }
+
+const modalInstance = basicLightbox.create(`
+     <div class="modal"> 
+         <li class="gallery_item">
+            <img 
+                src="${galleryItem.original}" 
+                class="gallery_image"
+                data-source="${galleryItem.original}"
+                alt="${galleryItem.description}"
+            />
+        </li>
+     </div>
+`)
+    modalInstance.show()
+
+    const handlerEsc = (event) => {
+     
+        if (event.code === "Escape") {
+            modalInstance.close();
+            listEl.removeEventListener("keydown", handlerEsc);
+        }
+        
+    };
+
+    listEl.addEventListener("keydown", handlerEsc);
+  
+    const containerModal = document.querySelector(".modal");
+    containerModal.style.width = `90%`;   
+    
+}
+
+renderList(galleryItems, listEl);
+listEl.addEventListener("click", imgGalleryOnClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const container = document.querySelector(".gallery");
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+
+function createMarkup(arr) {
+    return arr
+      .map(
+        ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+      )
+      .join("");
+  }
+
+  var lightbox = new SimpleLightbox('.gallery__item a', 
+  {  
+  captionsData: "alt",
+  captionDelay: 250
+});
